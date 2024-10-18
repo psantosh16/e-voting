a8bceb75ccac42415a52ff91acf9372e46c183b5
psantosh16 <santoshphadtare1610@gmail.com>
2024-10-19 00:34:18 +0530

    Login ui

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Login = (props) => {
     return (
         <div className="login-container">
-            <h1 className="text-6xl">🇮🇳</h1>
+            <h1 className="text-6xl hover:scale-150 ease-in-out	 transform">🇮🇳</h1>
             <h1 className="text-2xl my-4 font-bold">Welcome to decentralized voting application</h1>
-            <button className="px-4 py-2 bg-blue-600 text-white rounded-xl" onClick = {props.connectWallet}>Login Metamask</button>
+            <button className="px-4 py-2 bg-blue-600 text-white rounded-xl font-bold" onClick = {props.connectWallet}>Login Metamask</button>
         </div>
     )
 }
